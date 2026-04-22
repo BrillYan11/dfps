d193ef4df5916020a62f0fa4ac4870f9674f9994
Bryan Malinao <bnmalinao@up.edu.ph>
2026-04-22 08:09:43 +0800

    Fix strict_types declaration order

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 
 declare(strict_types=1);
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 
 $autoloadPath = __DIR__ . '/vendor/autoload.php';
 
